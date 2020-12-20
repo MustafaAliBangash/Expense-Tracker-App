03afdb22b8c0b9db5454b191b0822427491b1971
MustafaAliBangash <MustafaAliBangash@github.com>
2020-12-20 13:21:03 +0500

    Creating expense tracker app

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,41 @@
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
+
+  return (
+    <>
+      <h3>ADD NEW TRANSACTION</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">ENTER DESCRIPTION:</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >ENTER AMOUNT: <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">ADD TRANSACTION</button>
+      </form>
+    </>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <h2>
+      EXPENSE TRACKER
+    </h2>
+  )
+}
